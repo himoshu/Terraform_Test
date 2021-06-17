dda62ec511317361a84bd70e84a146cf09668562
himoshu <86024692+himoshu@users.noreply.github.com>
2021-06-17 11:45:43 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ variable "ARM_TENANT_ID" {}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-    SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_ID
-    CLIENT_ID = var.ARM_CLIENT_ID
-    CLIENT_SECRET = var.ARM_CLIENT_SECRET
-    TENANT_ID = var.ARM_TENANT_ID
+    ARM_SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_ID
+    ARM_CLIENT_ID = var.ARM_CLIENT_ID
+    ARM_CLIENT_SECRET = var.ARM_CLIENT_SECRET
+    ARM_TENANT_ID = var.ARM_TENANT_ID
     version = "~>2.50.0"
     features {}
 }
