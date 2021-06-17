188c3b79fe1cb0b48d6ce97c7d80c49e0a4bc331
himoshu <86024692+himoshu@users.noreply.github.com>
2021-06-18 03:08:33 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
     tenant_id = "${var.tenant_id}"
     client_id = "${var.client_id}"
     client_secret = "${var.client_secret}"
-    version = "~>2.50.0"
+    version = "~>6.13.1"
     features {}
 }
 
