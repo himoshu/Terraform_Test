e7407a9dba488c6ed63cd0950f5d646d9f791ea6
himoshu <86024692+himoshu@users.noreply.github.com>
2021-06-18 02:45:06 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-variable ARM_SUBSCRIPTION_ID=${id}
-variable ARM_CLIENT_ID=${appID}
-variable ARM_CLIENT_SECRET=${password}
-variable ARM_TENANT_ID=${tenantId}
+variable ARM_SUBSCRIPTION_ID = ${id}
+variable ARM_CLIENT_ID = ${appID}
+variable ARM_CLIENT_SECRET = ${password}
+variable ARM_TENANT_ID = ${tenantId}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
