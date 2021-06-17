cf8a4e4be4c8025def12489821cf22df6f50580e
himoshu <86024692+himoshu@users.noreply.github.com>
2021-06-17 10:14:02 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-variable ARM_SUBSCRIPTION_ID="<your subscription id>" {}
-variable ARM_CLIENT_ID="<your client id>" {}
-variable ARM_CLIENT_SECRET="<your client secret>" {}
-variable ARM_TENANT_ID="<your tenant id>" {}
+export ARM_SUBSCRIPTION_ID="<your subscription id>" {}
+export ARM_CLIENT_ID="<your client id>" {}
+export ARM_CLIENT_SECRET="<your client secret>" {}
+export ARM_TENANT_ID="<your tenant id>" {}
 
 
 
